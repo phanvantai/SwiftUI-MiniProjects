ae4b705e752ca36e6eb3a02f6d7c165c9b753758
Tai Phan Van <phanvantai.sh@gmail.com>
2021-08-02 09:47:53 +0700

    Init fruits project

---
+++ NRFruits/NRFruits/NRFruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NRFruitsApp.swift
+//  NRFruits
+//
+//  Created by Tai Phan Van on 02/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct NRFruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
