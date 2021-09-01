4c0a02a935a62fd453ad79ce133b1a81ef41148a
Tai Phan Van <phanvantai.sh@gmail.com>
2021-09-01 22:45:42 +0700

    init animal project

---
+++ AnimalWorld/AnimalWorld/AnimalWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalWorldApp.swift
+//  AnimalWorld
+//
+//  Created by Tai Phan Van on 01/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimalWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
